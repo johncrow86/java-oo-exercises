78d349a991a0ad49714f92d767e775adc6f8e2b0
John Crow <johncrow86@hotmail.com>
2016-09-29 13:53:31 -0500

    unit 3 studio 1

---
+++ java-track/studios/BaseballPlayer.java
@@ -0,0 +1,55 @@
+
+public class BaseballPlayer {
+	
+	private String name;
+	private int jerseyNumber;
+	private String swingStyle;
+	private int hits;
+	private int RBIs;
+	private int gamesPlayed;
+	
+	public BaseballPlayer(String name, int jerseyNumber, String swingStyle) {
+		this.name = name;
+		this.jerseyNumber = jerseyNumber;
+		this.swingStyle = swingStyle;
+		this.hits = 0;
+		this.RBIs = 0;
+		this.gamesPlayed = 0;
+	}
+	
+	public BaseballPlayer(String name, int jerseyNumber) {
+		this(name, jerseyNumber, "none");
+	}
+
+	public String toString() {
+		return "Name: " + name +
+				"\nJersey Number: " + jerseyNumber +
+				"\nSwing Style: " + swingStyle +
+				"\nHits: " + hits +
+				"\nRBIs: " + RBIs +
+				"\nTotal Games Played: " + gamesPlayed;
+	}
+	
+	public void addHits(int h) {
+		hits += h;
+	}
+	
+	public void addRBIs(int r) {
+		RBIs += r;
+	}
+	
+	public void addGamePlayed() {
+		gamesPlayed++;
+	}
+	
+	
+	public static void main(String[] args) {
+		BaseballPlayer p = new BaseballPlayer("John", 76, "left");
+		p.swingStyle = "right";
+		p.addHits(3);
+		p.addRBIs(5);
+		p.addGamePlayed();
+		System.out.println(p);
+	}
+
+}
